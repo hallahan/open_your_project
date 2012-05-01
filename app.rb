bebda4e7ae69ee02043647764a4b08f3af9eb90d
Harlan <harlan@thunder.local>
2012-05-01 15:57:12 -0700

    D3 circle packing data viz in the sidebar, with static sample data

--- app.rb
+++ app.rb
@@ -4,3 +4,79 @@ require 'haml'
 get '/' do
   haml :home
 end
+
+get '/network-viz.json' do
+  content_type 'application/json'
+  %~
+    {
+      "name": "farm",
+      "children": [
+        {
+          "name": "bar.noah.lvh.me",
+          "children": [
+            {
+              "name": "Welcome Visitors",
+              "size": "1"
+            }
+          ]
+        },
+        {
+          "name": "foo.noah.lvh.me",
+          "children": [
+            {
+              "name": "Welcome Visitors",
+              "size": "1"
+            }
+          ]
+        },
+        {
+          "name": "localhost",
+          "children": [
+            {
+              "name": "Air Temperature",
+              "size": "1"
+            },
+            {
+              "name": "D3 Bars",
+              "size": "1"
+            },
+            {
+              "name": "D3 Bars",
+              "size": "1"
+            },
+            {
+              "name": "D3 Line",
+              "size": "1"
+            },
+            {
+              "name": "Welcome Visitors",
+              "size": "1"
+            }
+          ]
+        },
+        {
+          "name": "lvh.me",
+          "children": [
+            {
+              "name": "D3 Line",
+              "size": "1"
+            },
+            {
+              "name": "Welcome Visitors",
+              "size": "1"
+            }
+          ]
+        },
+        {
+          "name": "noah.lvh.me",
+          "children": [
+            {
+              "name": "Welcome Visitors",
+              "size": "1"
+            }
+          ]
+        }
+      ]
+    }
+  ~
+end
