9f8529900f4a1c7f763e91fa88a62816b50e2456
Harlan T Wood <harlan@thegoldensun.com>
2012-05-05 05:07:15 -0700

    Switch to "pack hierarchy" viz, from https://github.com/mbostock/d3/blob/a6c02987b87f16fb1b67f36c32ed252dd13b96b7/talk/20111116/pack-hierarchy.html

--- public/javascripts/viz/tags.js
+++ public/javascripts/viz/tags.js
@@ -1,36 +1,36 @@
-d3.json("/viz/tags.json", function(json) {
-
-  var width = 250,
-      height = 250,
-      format = d3.format(",d");
-
-  var pack = d3.layout.pack()
-      .size([width - 4, height - 4])
-      .value(function(d) { return d.size; });
-
-  var vis = d3.select("#viz-tags").append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("class", "pack")
-    .append("g")
-      .attr("transform", "translate(2, 2)");
-
-
-  var node = vis.data([json]).selectAll("#viz-tags g.node")
-      .data(pack.nodes)
-    .enter().append("g")
-      .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-
-  node.append("title")
-      .text(function(d) { return d.name + (d.children ? "" : ": " + format(d.size)); });
-
-  node.append("circle")
-      .attr("r", function(d) { return d.r; });
-
-  node.filter(function(d) { return !d.children; })
-    .append("text")
-      .attr("text-anchor", "middle")
-      .attr("dy", ".3em")
-      .text(function(d) { return d.name.substring(0, d.r / 3); });
-});
+//d3.json("/viz/tags.json", function(json) {
+//
+//  var width = 250,
+//      height = 250,
+//      format = d3.format(",d");
+//
+//  var pack = d3.layout.pack()
+//      .size([width - 4, height - 4])
+//      .value(function(d) { return d.size; });
+//
+//  var vis = d3.select("#viz-tags").append("svg")
+//      .attr("width", width)
+//      .attr("height", height)
+//      .attr("class", "pack")
+//    .append("g")
+//      .attr("transform", "translate(2, 2)");
+//
+//
+//  var node = vis.data([json]).selectAll("#viz-tags g.node")
+//      .data(pack.nodes)
+//    .enter().append("g")
+//      .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
+//      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+//
+//  node.append("title")
+//      .text(function(d) { return d.name + (d.children ? "" : ": " + format(d.size)); });
+//
+//  node.append("circle")
+//      .attr("r", function(d) { return d.r; });
+//
+//  node.filter(function(d) { return !d.children; })
+//    .append("text")
+//      .attr("text-anchor", "middle")
+//      .attr("dy", ".3em")
+//      .text(function(d) { return d.name.substring(0, d.r / 3); });
+//});
