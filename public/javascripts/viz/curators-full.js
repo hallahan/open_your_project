0e08b644a1b2b4837888e72154d51e47ca18d531
Harlan T Wood <harlan@thegoldensun.com>
2012-05-05 22:52:08 -0700

    Move zoom into main function, so that it shares the local variables; Minor js viz appearance tweaks

--- public/javascripts/viz/curators-full.js
+++ public/javascripts/viz/curators-full.js
@@ -1,6 +1,6 @@
 d3.json(curators_json_path, function(data) {
 
-  var w = 1280,
+  var w = 800,
       h = 800,
       r = 720,
       x = d3.scale.linear().range([0, r]),
@@ -39,30 +39,32 @@ d3.json(curators_json_path, function(data) {
       .attr("y", function(d) { return d.y; })
       .attr("dy", ".35em")
       .attr("text-anchor", "middle")
-      .style("opacity", function(d) { return d.r > 20 ? 1 : 0; })
+      .style("opacity", function(d) { return 1; })
       .text(function(d) { return d.children ? d.name : ''; });
 
   d3.select(window).on("click", function() { zoom(root); });
+
+  function zoom(d, i) {
+    var k = r / d.r / 2;
+    x.domain([d.x - d.r, d.x + d.r]);
+    y.domain([d.y - d.r, d.y + d.r]);
+
+    var t = vis.transition()
+        .duration(d3.event.altKey ? 7500 : 750);
+
+    t.selectAll("circle")
+        .attr("cx", function(d) { return x(d.x); })
+        .attr("cy", function(d) { return y(d.y); })
+        .attr("r", function(d) { return k * d.r; });
+
+    t.selectAll("text")
+        .attr("x", function(d) { return x(d.x); })
+        .attr("y", function(d) { return y(d.y); })
+        .style("opacity", function(d) { return k * d.r > 20 ? 1 : 0; });
+
+    node = d;
+    d3.event.stopPropagation();
+  }
+
 });
 
-function zoom(d, i) {
-  var k = r / d.r / 2;
-  x.domain([d.x - d.r, d.x + d.r]);
-  y.domain([d.y - d.r, d.y + d.r]);
-
-  var t = vis.transition()
-      .duration(d3.event.altKey ? 7500 : 750);
-
-  t.selectAll("circle")
-      .attr("cx", function(d) { return x(d.x); })
-      .attr("cy", function(d) { return y(d.y); })
-      .attr("r", function(d) { return k * d.r; });
-
-  t.selectAll("text")
-      .attr("x", function(d) { return x(d.x); })
-      .attr("y", function(d) { return y(d.y); })
-      .style("opacity", function(d) { return k * d.r > 20 ? 1 : 0; });
-
-  node = d;
-  d3.event.stopPropagation();
-}
