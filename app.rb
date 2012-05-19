037edcc6b107654d3b4f1d8a767f567ec149f1d1
Harlan T Wood <harlan@thegoldensun.com>
2012-05-19 02:29:23 -0700

    Move static assets from public to app

--- app.rb
+++ app.rb
@@ -1,29 +1,16 @@
 require 'active_support/core_ext'
-#require 'formtastic'
 require 'haml'
 require 'json'
 require 'nokogiri'
 require 'rest_client'
 require 'sinatra'
 
-#require 'sinatra_more/markup_plugin'
-
 Dir[File.expand_path("lib/**/*.rb", File.dirname(__FILE__))].each { |lib| require lib }
-require (File.expand_path 'fork', File.dirname(__FILE__))
-
-class Controller < Sinatra::Base
-
-#register SinatraMore::MarkupPlugin
 
 enable :logging, :dump_errors, :raise_errors
 set :show_exceptions, true if development?
 
 raise "Please set the environment variable 'SFW_BASE_DOMAIN'" if ENV['SFW_BASE_DOMAIN'].nil? || ENV['SFW_BASE_DOMAIN'].empty?
-
-def form_pre_filled?
-  development?
-  false
-end
 
 def development?
   ENV['RACK_ENV'] == 'development'
@@ -39,27 +26,18 @@ get '/' do
 end
 
 post '/' do
-  @fork = Fork.new(params)
-  p 222, @fork.valid?
-  (haml :home && return) if !@fork.valid?
-
-  html = RestClient.get @fork.url
+  url = params[:url] =~ %r{^https?://} ? params[:url] : "http://#{params[:url]}"
+  html = RestClient.get url
   doc = Nokogiri::HTML(html)
-  begin
-    fork_url = FedWiki.open(doc, @fork.url, :username => @fork.username, :topic => @fork.topic)
-    redirect fork_url
-  rescue InterWiki::NoKnownOpenLicense
-    # add error to @fork
-    haml :home
-  end
+  options = params.symbolize_keys.slice(:username, :topic)
+  fork_url = FedWiki.open(doc, url, options)
+  redirect fork_url
 end
 
 get %r{^/viz/(\w+)$} do |viz|
   @viz = viz
   @json_path = "http://sfw.#{ENV['SFW_BASE_DOMAIN']}/viz/#{viz}.json"
   haml viz.to_sym
-end
-
 end
 
 #get '/curators' do
