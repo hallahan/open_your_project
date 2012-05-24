bac88cb6afef1b62dd10e182a114c7859569eb4f
Harlan T Wood <harlan@thegoldensun.com>
2012-05-23 23:17:49 -0700

    String#slug - more posix character classes; remove apostrophes; integrate existing specs into new spec format

--- lib/ruby_extensions/string.rb
+++ lib/ruby_extensions/string.rb
@@ -10,29 +10,28 @@ class String
   def slug(sep = '-')
     massaged = self.dup
 
+    # Massage path-like segments
+
     massaged.gsub!(/#.*$/, '')               # strip off anchor tags, eg #section-2
     massaged.gsub!(/\?.*$/, '')              # strip off query sting, eg ?cid=6a0
     massaged.gsub!(/\.[a-z]{3,10}$/i, '')     # strip off file extensions, eg .html
 
-    match = massaged.match %r[
-      ^
-        (
-          /\d{4}/\d{2}    # optional leading date stamp, eg 2012/12
-          (?:/\d{2})?     #                              or 2012/12/21
-        )?
-        (.*?[a-z].*?)     # require at least one alpha char for slug, if we are stripping the date
-      $
-    ]xi
-    massaged = match[2] if (match && match[1] && match[2])   # strip leading date
+    massaged.gsub! %r[
+      /\d{4}/\d{2}           # optional leading date stamp, eg /2012/12/great-post    or /blog/2012/12/great-post
+      (?:/\d{2})?            #                              or /2012/12/21/           or /blog/brian/2012/12/21/great-post
+      (?=/.*?[[:alpha:]])    # require at least one alpha char after the date (for the slug)
+    ]x, ''
 
     massaged = 'home' if massaged.match(%r{^/?$})
 
-    # Below is a simplified version of the code found in rails 3.2.1:
-    # activesupport/lib/active_support/inflector/transliterate.rb
-    # Note that the transliteration to ASCII has been removed for simplicity
+    # Remove single quotes within words, eg O'Malley -> OMalley, or Don't -> Dont
+
+    massaged.gsub!(/(?<=[[:alpha:]])'(?=[[:alpha:]])/, '')
+
+    # Replace unsupported chars with 'sep'
 
     massaged.downcase!
-    massaged.gsub!(/[^a-z0-9\-]+/, sep)  # differs from rails version: don't allow underscores
+    massaged.gsub!(/[^[[:alnum:]]-]+/, sep)
     unless sep.nil? || sep.empty?
       re_sep = Regexp.escape(sep)
       # No more than one of the separator in a row.
--- spec/lib/ruby_extensions/string_spec.rb
+++ spec/lib/ruby_extensions/string_spec.rb
@@ -2,29 +2,33 @@
 require File.expand_path("../../../lib/ruby_extensions/string", File.dirname(__FILE__))
 
 def section (comment)
-  puts "\n\t#{comment}\n"
+  $section = comment
+  $counter = 0
 end
 
 def test (given, expected)
-  actual = given.slug
-  puts actual == expected ? "OK\t#{given}" : "YIKES\t#{given} => #{actual}, not #{expected} as expected"
+  describe "#{$section}: Test ##{$counter+=1}" do
+    it "should convert the string #{given.inspect} to the slug #{expected.inspect}" do
+      given.slug.should == expected
+    end
+  end
 end
 
 # the following test cases presume to be implementation language agnostic
 # perhaps they should be included from a common file
 
-# 'WORKING'
 section 'case and hyphen insensitive'
 test 'Welcome Visitors', 'welcome-visitors'
 test 'welcome visitors', 'welcome-visitors'
 test 'Welcome-visitors', 'welcome-visitors'
 
-
 section 'numbers and punctuation'
 test '2012 Report', '2012-report'
 test 'Ward\'s Wiki', 'wards-wiki'
+test 'holy cats !!! you don\'t say', 'holy-cats-you-dont-say'
+test 'Pride & Prejudice', 'pride-prejudice'
+test '---holy cats !!! ---------', 'holy-cats'
 
-# 'PROBLEMATIC'
 section 'white space insenstive'
 test 'Welcome  Visitors', 'welcome-visitors'
 test '  Welcome Visitors', 'welcome-visitors'
@@ -34,65 +38,13 @@ section 'foreign language'
 test 'Les Misérables', 'les-misérables'
 test 'Les Misérables', 'les-misérables'
 
-#test '/2020/20', '2020-20'
-#test '/2020/20/', '2020-20'
-#test '/2020/20/22', '2020-20-22'
-#test '/2020/20/22/1', '2020-20-22-1'
-#test '/2020/20/22/a', 'a'
-#test '/2020/20/22/foo/bar', 'foo-bar'
-#test '/2020/20/foo/bar', 'foo-bar'
-#test '/wiki/Superintelligence', 'wiki-superintelligence'
-
-
-#describe String do
-#  describe '#slug' do
-#    %w[
-#      /2020/20                   2020-20
-#      /2020/20/                  2020-20
-#      /2020/20/22                2020-20-22
-#      /2020/20/22/1              2020-20-22-1
-#      /2020/20/22/a              a
-#      /2020/20/22/foo/bar        foo-bar
-#      /2020/20/foo/bar           foo-bar
-#      /wiki/Superintelligence    wiki-superintelligence
-#    ].
-#    each_slice(2) do |old, expected|
-#      puts "test '#{old}', '#{expected}'"
-#    end
-#
-#
-#    [
-#      "Pride & Prejudice",             "pride-prejudice",
-#      "holy cats !!! you don't say",   "holy-cats-you-don-t-say",
-#      "---holy cats !!! ---------",    "holy-cats",
-#    ].
-#    each_slice(2) do |title, expected_slug|
-#      it "should convert the title #{title.inspect} to the slug #{expected_slug.inspect}" do
-#        title.slug.should == expected_slug
-#      end
-#    end
-#
-#    # 'WORKING'
-#    section 'case and hyphen insensitive'
-#    test 'Welcome Visitors', 'welcome-visitors'
-#    test 'welcome visitors', 'welcome-visitors'
-#    test 'Welcome-visitors', 'welcome-visitors'
-#
-#
-#    section 'numbers and punctuation'
-#    test '2012 Report', '2012-report'
-#    test 'Ward\'s Wiki', 'wards-wiki'
-#
-#    # 'PROBLEMATIC'
-#    section 'white space insenstive'
-#    test 'Welcome  Visitors', 'welcome-visitors'
-#    test '  Welcome Visitors', 'welcome-visitors'
-#    test 'Welcome Visitors  ', 'welcome-visitors'
-#
-#    section 'foreign language'
-#    test 'Les Misérables', 'les-misérables'
-#    test 'Les Misérables', 'les-miserables'
-#
-#  end
-#end
-#
+section 'path-like segments'
+test '/2020/20', '2020-20'
+test '/2020/20/', '2020-20'
+test '/2020/20/22', '2020-20-22'
+test '/2020/20/22/-/-/---', '2020-20-22'
+test '/2020/20/22/1', '2020-20-22-1'
+test '/2020/20/22/a', 'a'
+test '/2020/20/22/foo/bar', 'foo-bar'
+test '/2020/20/foo/bar', 'foo-bar'
+test '/wiki/Superintelligence', 'wiki-superintelligence'
