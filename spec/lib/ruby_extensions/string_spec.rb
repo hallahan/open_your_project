d29f63c7d4c167042af708018dce5c19b8a4da6e
Harlan T Wood <harlan@thegoldensun.com>
2012-05-23 21:37:14 -0700

    Specs for String#slug

--- spec/lib/ruby_extensions/string_spec.rb
+++ spec/lib/ruby_extensions/string_spec.rb
@@ -2,23 +2,54 @@ require File.expand_path("../../../lib/ruby_extensions/string", File.dirname(__F
 
 describe String do
   describe '#slug' do
-    it 'should convert a path to a slug' do
-      '/wiki/Superintelligence'.slug.should == 'wiki-superintelligence'
+    %w[
+      /2020/20                   2020-20
+      /2020/20/                  2020-20
+      /2020/20/22                2020-20-22
+      /2020/20/22/1              2020-20-22-1
+      /2020/20/22/a              a
+      /2020/20/22/foo/bar        foo-bar
+      /2020/20/foo/bar           foo-bar
+      /wiki/Superintelligence    wiki-superintelligence
+    ].
+    each_slice(2) do |path, expected_slug|
+      it "should convert the path #{path.inspect} to the slug #{expected_slug.inspect}" do
+        path.slug.should == expected_slug
+      end
     end
+
+    [
+      "Pride & Prejudice",             "pride-prejudice",
+      "holy cats !!! you don't say",   "holy-cats-you-don-t-say",
+      "---holy cats !!! ---------",    "holy-cats",
+    ].
+    each_slice(2) do |title, expected_slug|
+      it "should convert the title #{title.inspect} to the slug #{expected_slug.inspect}" do
+        title.slug.should == expected_slug
+      end
+    end
+
+    # 'WORKING'
+    section 'case and hyphen insensitive'
+    test 'Welcome Visitors', 'welcome-visitors'
+    test 'welcome visitors', 'welcome-visitors'
+    test 'Welcome-visitors', 'welcome-visitors'
+
+
+    section 'numbers and punctuation'
+    test '2012 Report', '2012-report'
+    test 'Ward\'s Wiki', 'wards-wiki'
+
+    # 'PROBLEMATIC'
+    section 'white space insenstive'
+    test 'Welcome  Visitors', 'welcome-visitors'
+    test '  Welcome Visitors', 'welcome-visitors'
+    test 'Welcome Visitors  ', 'welcome-visitors'
+
+    section 'foreign language'
+    test 'Les Misérables', 'les-misérables'
+    test 'Les Misérables', 'les-miserables'
+
   end
 end
 
-#"/2020/20"
-#"/2020/20/"
-#"/2020/20/22"
-#"/2020/20/22/1"
-#"/2020/20/22/foo/bar"
-#"/2020/20/foo/bar"
-#"2020-20"
-#"2020-20-22"
-#"2020-20-22-1"
-#"2020-20-22-foo-bar"
-#"2020-20-foo-bar"
-#"holy fuck !!! you are good"
-#"holy fuck !!! you are good---------"
-#"holy-fuck-you-are-good"
